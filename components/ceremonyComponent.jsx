98b0434788e0e54c5332aa477a21efac7dd19a10
Angelicabel <angelicabel92@gmail.com>
2020-08-21 15:54:03 +0200

    sop

--- components/ceremonyComponent.jsx
+++ components/ceremonyComponent.jsx
@@ -4,20 +4,19 @@ const CeremonyComponent = () => {
     return (
         <div>
             <Row className='ceremory-component align-items-center mt-5 pt-3'>
-                <Col lg='12' className='text-center'>
+                <Col lg='12' className='text-center mt-2 mb-5'>
                     <p className='ceremory-component__intro'>Quiero invitarlos a mi bautizo, por razones que ya sabemos
                         no podemos celebrarlo en grande así que se nos ocurrió la idea de que todos
                         podamos participar
                     </p>
                 </Col>
-                <Col lg='6' className="text-center mb-3">
+                <Col lg='6' className="text-center">
                     <div className='d-flex align-items-center justify-content-center ceremory-component__date'>
-                        <span className="ceremory-component__date--number mb-3">30</span>
+                        <span className="ceremory-component__date--number mr-3">30</span>
                         <span className="ceremory-component__date--month">Agosto <br/> de 2020</span>
                     </div>
                     <p className="ceremory-component__hour">Hora: 10:00 AM</p>
-                    <a className="btn btn-lg ceremory-component__btn" href="https://meet.google.com/xeq-nkpp-ozy" target='_blank'>
-                        Ir a la capilla</a>
+                    <a className="btn btn-lg ceremory-component__btn" href="https://meet.google.com/xeq-nkpp-ozy" target='_blank'> Ir a la capilla</a>
                 </Col>
                 <Col lg='6' className='ceremory-component__img'>
                     <Image src="./familia-bm.jpeg" rounded />
@@ -30,7 +29,7 @@ const CeremonyComponent = () => {
                 </Col>
                 <Col lg='6'>
                     <h4 className='ceremory-component__title'>Mis papis</h4>
-                    <div className='d-flex justify-content-around  mt-4'>
+                    <div className='d-flex justify-content-around flex-wrap mt-4'>
                         <div className='d-flex ceremory-component__retr mb-4'>
                             <Image className='ceremory-component__retr--img' src="./jessica.png" />
                             <p>Jessica Martinez</p>
@@ -43,7 +42,7 @@ const CeremonyComponent = () => {
                 </Col>
                 <Col lg='6'>
                     <h4 className='ceremory-component__title'>Mis padrinos</h4>
-                    <div className='d-flex justify-content-around mt-4'>
+                    <div className='d-flex justify-content-around flex-wrap mt-4'>
                         <div className='d-flex ceremory-component__retr mb-4'>
                             <Image className='ceremory-component__retr--img' src="./angelica.png" />
                             <p>Angélica Beltrán</p>
--- pages/about.jsx
+++ pages/about.jsx
@@ -16,27 +16,27 @@ const AboutPage = () => {
                 <div className="row">
                     <div className="col-md-12 text-center mb-5 mt-5">
                         <h3 className='mb-3'>¡Mi invitación personalizada!</h3>
-                        <iframe src="https://www.youtube.com/embed/bZKA5ZOBBF0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/bZKA5ZOBBF0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <p className='mt-2 mb-3 text-center'> ¡Estos son algunos recuerdos y momentos que me gustaría recordar con ustedes!</p>
                     </div>
-                    <div className="col-md-6 text-center mb-3">
-                        <iframe src="https://www.youtube.com/embed/RA6igk-9wMg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div className="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/RA6igk-9wMg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <p>Sopi de bebé</p>
                     </div>
-                    <div className="col-md-6 text-center mb-3">
-                        <iframe src="https://www.youtube.com/embed/Sq89_6pgpSU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div className="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/Sq89_6pgpSU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <p>Sopi viendo la tv</p>
                     </div>
-                    <div className="col-md-6 text-center mb-3">
-                        <iframe src="https://www.youtube.com/embed/Htic4LpES1c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div className="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/Htic4LpES1c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <p>Sopi ayudando en casa</p>
                     </div>
-                    <div className="col-md-6 text-center mb-3">
-                        <iframe src="https://www.youtube.com/embed/huV4N2JjeVE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div className="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/huV4N2JjeVE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <p>Sopi celebrando el cumple de la abuelita Alicia</p>
                     </div>
-                    <div className="col-md-6 text-center mb-3">
-                        <iframe src="https://www.youtube.com/embed/p43NrJFJ1YY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div className="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/p43NrJFJ1YY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <p>¿Cómo hace el gatito?</p>
                     </div>
                 </div>

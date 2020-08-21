2e1ddca7d3dd211436958db088213cf5839b6a46
Angelicabel <angelicabel92@gmail.com>
2020-08-21 15:49:59 +0200

    sopi

--- components/ceremonyComponent.jsx
+++ components/ceremonyComponent.jsx
@@ -4,19 +4,20 @@ const CeremonyComponent = () => {
     return (
         <div>
             <Row className='ceremory-component align-items-center mt-5 pt-3'>
-                <Col lg='12' className='text-center mt-2 mb-5'>
+                <Col lg='12' className='text-center'>
                     <p className='ceremory-component__intro'>Quiero invitarlos a mi bautizo, por razones que ya sabemos
                         no podemos celebrarlo en grande así que se nos ocurrió la idea de que todos
                         podamos participar
                     </p>
                 </Col>
-                <Col lg='6' className="text-center">
+                <Col lg='6' className="text-center mb-3">
                     <div className='d-flex align-items-center justify-content-center ceremory-component__date'>
-                        <span className="ceremory-component__date--number mr-3">30</span>
+                        <span className="ceremory-component__date--number mb-3">30</span>
                         <span className="ceremory-component__date--month">Agosto <br/> de 2020</span>
                     </div>
                     <p className="ceremory-component__hour">Hora: 10:00 AM</p>
-                    <a className="btn btn-lg ceremory-component__btn" href="https://meet.google.com/xeq-nkpp-ozy" target='_blank'> Ir a la capilla</a>
+                    <a className="btn btn-lg ceremory-component__btn" href="https://meet.google.com/xeq-nkpp-ozy" target='_blank'>
+                        Ir a la capilla</a>
                 </Col>
                 <Col lg='6' className='ceremory-component__img'>
                     <Image src="./familia-bm.jpeg" rounded />
@@ -29,7 +30,7 @@ const CeremonyComponent = () => {
                 </Col>
                 <Col lg='6'>
                     <h4 className='ceremory-component__title'>Mis papis</h4>
-                    <div className='d-flex justify-content-around flex-wrap mt-4'>
+                    <div className='d-flex justify-content-around  mt-4'>
                         <div className='d-flex ceremory-component__retr mb-4'>
                             <Image className='ceremory-component__retr--img' src="./jessica.png" />
                             <p>Jessica Martinez</p>
@@ -42,7 +43,7 @@ const CeremonyComponent = () => {
                 </Col>
                 <Col lg='6'>
                     <h4 className='ceremory-component__title'>Mis padrinos</h4>
-                    <div className='d-flex justify-content-around flex-wrap mt-4'>
+                    <div className='d-flex justify-content-around mt-4'>
                         <div className='d-flex ceremory-component__retr mb-4'>
                             <Image className='ceremory-component__retr--img' src="./angelica.png" />
                             <p>Angélica Beltrán</p>

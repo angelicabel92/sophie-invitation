0a66d9f6e72fd026e017ae2f823489887e252520
Angelica Beltran <angelica.beltran@selectra.com>
2020-08-24 12:34:53 +0200

    fix invitation

--- pages/about.jsx
+++ pages/about.jsx
@@ -14,10 +14,8 @@ const AboutPage = () => {
                 <HeaderComponent />
                 <NavComponent />
                 <div className="row">
-                    <div className="col-md-12 text-center mb-5 mt-5">
-                        <h3 className='mb-3'>¡Mi invitación personalizada!</h3>
-                        <iframe src="https://www.youtube.com/embed/bZKA5ZOBBF0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <p className='mt-2 mb-3 text-center'> ¡Estos son algunos recuerdos y momentos que me gustaría recordar con ustedes!</p>
+                    <div className="col-md-12  mt-5 mb-4">
+                        <h3 className='mt-2 mb-3 text-center'>¡Estos son algunos recuerdos y momentos que me gustaría recordar con ustedes!</h3>
                     </div>
                     <div className="col-md-6  text-center mb-3">
                         <iframe src="https://www.youtube.com/embed/RA6igk-9wMg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -32,12 +30,16 @@ const AboutPage = () => {
                         <p>Sopi ayudando en casa</p>
                     </div>
                     <div className="col-md-6  text-center mb-3">
-                        <iframe src="https://www.youtube.com/embed/huV4N2JjeVE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <p>Sopi celebrando el cumple de la abuelita Alicia</p>
+                        <iframe src="https://www.youtube.com/embed/hgUziyP_ijk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <p>Sopi aprendiendo a comer</p>
                     </div>
                     <div className="col-md-6 text-center mb-3">
                         <iframe src="https://www.youtube.com/embed/p43NrJFJ1YY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <p>¿Cómo hace el gatito?</p>
+                    </div>
+                    <div className="col-md-6 text-center mb-3">
+                        <iframe src="https://www.youtube.com/embed/SE7x1niuPj8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <p>Sophi cepillando sus dientes</p>
                     </div>
                 </div>
 
